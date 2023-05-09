6fff0e23db40049c197ff4321a75887ea1680a14
DESKTOP-RFHCBQP\akhma <akhmadullin23@gmail.com>
2023-05-09 15:58:19 +0200

    - Added weapon class and blueprint - Added weapon meshes and sounds

---
+++ Source/MenuSystem/Character/ShootingCharacterAnimInstance.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShootingCharacterAnimInstance.h"
+#include "KismetAnimationLibrary.h"
+#include "ShootingCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShootingCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ShootingCharacter = Cast<AShootingCharacter>(TryGetPawnOwner());
+}
+
+void UShootingCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (ShootingCharacter == nullptr)
+	{
+		ShootingCharacter = Cast<AShootingCharacter>(TryGetPawnOwner());
+	}
+
+	if(ShootingCharacter == nullptr) return;
+
+	FVector Velocity = ShootingCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	FRotator Rotation = ShootingCharacter->GetBaseAimRotation();
+	FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	Direction = CalculateDirection(Velocity, YawRotation);
+
+	bIsInAir = ShootingCharacter->GetCharacterMovement()->IsFalling();
+
+	bIsAccelerating = ShootingCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true : false;
+	
+}
